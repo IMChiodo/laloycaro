c563d1ff5bdfe87550ee3e6c76ac175266f69890
IMChiodo <imchiodo1@gmail.com>
2023-11-01 21:33:16 -0300

    tinderlist

--- src/app/tinder-list/tinder-list.component.ts
+++ src/app/tinder-list/tinder-list.component.ts
@@ -4,13 +4,15 @@ import fullpage from 'fullpage.js';
 import FontFaceObserver from 'fontfaceobserver';
 import { CommonModule } from '@angular/common';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import * as bachelorsData from '../../bachelors.json';
+import { DivisionComponent } from '../division/division.component';
 
 @Component({
   standalone: true,
   selector: 'app-tinder-list',
   templateUrl: './tinder-list.component.html',
   styleUrls: ['./tinder-list.component.css'],
-  imports: [CommonModule],
+  imports: [CommonModule, DivisionComponent],
 })
 export class TinderListComponent implements OnInit {
   isLoading = true;
@@ -21,11 +23,7 @@ export class TinderListComponent implements OnInit {
   bachelors: Array<any> = [];
   public profilePicUrl: SafeUrl | string = '';
 
-  constructor(
-    private readonly mainService: MainService,
-    private readonly cd: ChangeDetectorRef,
-    private sanitizer: DomSanitizer // Inject Angular's DomSanitizer
-  ) {}
+  constructor(private readonly cd: ChangeDetectorRef) {}
 
   ngOnInit(): void {
     this.checkImagesLoaded();
@@ -48,69 +46,12 @@ export class TinderListComponent implements OnInit {
   }
 
   getBachelors(): void {
-    this.mainService.getBachelors().subscribe((data) => {
-      console.log(data);
+    console.log(JSON.stringify(bachelorsData));
+    this.bachelors = JSON.parse(JSON.stringify(bachelorsData)).default;
+    console.log(this.bachelors);
 
-      this.bachelors = data;
-      this.dataLoaded = true;
-      this.checkAllLoaded();
-
-      this.bachelors.forEach((bachelor) => {
-        this.mainService
-          .getInstagramPageHtml(bachelor.instagram)
-          .subscribe((data) => {
-            console.log(data);
-            const parsedHtml = this.parseHtml(data);
-            const parsed = JSON.parse(data);
-            const profilePicture = parsed.graphql.user.profile_pic_url_hd;
-            bachelor.picture = profilePicture;
-            this.dataLoaded = true;
-            this.checkAllLoaded();
-          });
-      });
-    });
-  }
-
-  parseHtml(html: string) {
-    // Use DOMParser to parse the HTML content
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(html, 'text/html');
-
-    // Find all script tags - note this might not be efficient depending on the page size
-    const scripts = Array.from(doc.querySelectorAll('script'));
-
-    // Find the script tag with the content we're interested in.
-    // This is based on current Instagram web structure as of my last training data in September 2021.
-    const targetScript = scripts.find((script) =>
-      script.innerHTML.includes('profile_pic_url_hd')
-    );
-
-    if (targetScript) {
-      // Extract the JSON from the script tag
-      const scriptContent = targetScript.innerHTML;
-
-      // The JSON data typically starts with 'window._sharedData =' in the script tag
-      const jsonStr = scriptContent
-        .split('window._sharedData =')[1]
-        .split('</script>')[0]
-        .trim()
-        .slice(0, -1); // Removing the trailing semicolon
-
-      try {
-        const jsonData = JSON.parse(jsonStr);
-        // Navigate through the JSON structure to get the profile pic URL
-        const profilePicUrl =
-          jsonData.entry_data.ProfilePage[0].graphql.user.profile_pic_url_hd;
-
-        // Since we're dealing with an external URL, we should sanitize it before using it in our application
-        this.profilePicUrl =
-          this.sanitizer.bypassSecurityTrustUrl(profilePicUrl);
-
-        console.log('Profile Picture URL:', this.profilePicUrl);
-      } catch (error) {
-        console.error('Error parsing JSON:', error);
-      }
-    }
+    this.dataLoaded = true;
+    this.checkAllLoaded();
   }
 
   checkImagesLoaded() {
